1514020318d14ba8706aa172ea1c978af10aa573
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-02-07 17:36:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Ajouter une donnée dans le local storage
+localStorage.setItem("nom", "Olivier Robert-Duboille");
+localStorage.setItem("age", 27);
+localStorage.setItem("email", "olivierrobertduboille@gmail.com");
+
+//Récupérer une donnée dans le localStorage
+
+let maDonnee = localStorage.getItem("nom");
+console.log(maDonnee);
